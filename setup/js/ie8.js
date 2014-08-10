16c541845766ac3a999256aa12fbf02086c8b653
Chris Carlevato <chris@christopherl.com>
2014-08-10 11:35:45 -0600

    Updates

--- setup/js/ie8.js
+++
@@ -1,7 +0,0 @@
-document.createElement('header');
-document.createElement('nav');
-document.createElement('section');
-document.createElement('article');
-document.createElement('footer');
-document.createElement('aside');
-document.createElement('time');
